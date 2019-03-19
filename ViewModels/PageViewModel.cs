de59b0115d2b8bfd59ccd90bf43ea2c362a4f439
Administrator <lagunamichael2@outlook.com>
2019-03-18 23:31:33 -0600

    edit1

--- ViewModels/PageViewModel.cs
+++ ViewModels/PageViewModel.cs
@@ -362,6 +362,8 @@ namespace Variedades
 
             _context.Entry(entity).CurrentValues.SetValues(item);
             _context.SaveChanges();
+
+            UpdateProducts(3);
         }
 
         //Modulo de borrado
