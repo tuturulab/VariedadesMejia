7b3057fce0cbb13d1f875d28f2795ef18195a62b
josesalasni <josesalasni@gmail.com>
2019-03-23 23:20:18 -0600

    Login sacaclavos
    monkas

--- Views/LoginWindow.xaml.cs
+++ Views/LoginWindow.xaml.cs
@@ -19,6 +19,8 @@ namespace Variedades
     /// </summary>
     public partial class LoginWindow : Window
     {
+        MainWindow mainWindow;
+
         public LoginWindow()
         {
             InitializeComponent();
@@ -27,7 +29,23 @@ namespace Variedades
         //Start main window
         private void LoginButton(object sender, RoutedEventArgs e)
         {
+            if (UserTextBox.Text == "admin" && PassTextBox.Password == "1234")
+            {
+                //Iniciamos la ventana de crear un producto
+                mainWindow = new MainWindow();
 
+                //Abrimos
+                mainWindow.Show();
+                this.Close();
+            }
+
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Datos Incorrectos, por favor intente nuevamente",
+                                                  "Confirmation",
+                                                  MessageBoxButton.OK,
+                                                  MessageBoxImage.Exclamation);
+            }
         }
     }
 }
