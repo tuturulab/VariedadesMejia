e0a331c8631f184c897341cb135867057ba7f692
Carlos José Salas <josesalasni@gmail.com>
2019-03-30 19:17:42 -0600

    end day update

--- Views/ImportToProduct.xaml.cs
+++ Views/ImportToProduct.xaml.cs
@@ -23,5 +23,57 @@ namespace Variedades.Views
         {
             InitializeComponent();
         }
+
+        public void BtnInsertarProductos (object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void BtnInsertarImportacion (object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBorrarClick (object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DataGrid_CellGotFocus(object sender, RoutedEventArgs e)
+        {
+            // Lookup for the source to be DataGridCell
+            if (e.OriginalSource.GetType() == typeof(DataGridCell))
+            {
+                // Starts the Edit on the row;
+                DataGrid grd = (DataGrid)sender;
+                grd.BeginEdit(e);
+
+                Control control = GetFirstChildByType<Control>(e.OriginalSource as DataGridCell);
+                if (control != null)
+                {
+                    control.Focus();
+                }
+            }
+        }
+
+        private T GetFirstChildByType<T>(DependencyObject prop) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(prop); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild((prop), i) as DependencyObject;
+                if (child == null)
+                    continue;
+
+                T castedProp = child as T;
+                if (castedProp != null)
+                    return castedProp;
+
+                castedProp = GetFirstChildByType<T>(child);
+
+                if (castedProp != null)
+                    return castedProp;
+            }
+            return null;
+        }
     }
 }
